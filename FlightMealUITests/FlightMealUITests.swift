132fc379f1f0c0cb7b96f15ff9359ff0479314ed
Mohammed Afsal <afsalkp.mohammed@gmail.com>
2023-12-31 11:46:29 +0400

    test target inluded in the scheme

--- FlightMealUITests/FlightMealUITests.swift
+++ FlightMealUITests/FlightMealUITests.swift
@@ -9,4 +9,18 @@ import XCTest
 
 @testable import FlightMeal
 
-class FlightMealUITests: XCTestCase {}
+class FlightMealUITests: XCTestCase {
+  
+  func testExample() throws {
+            let app = XCUIApplication()
+    app.launch()
+  }
+  
+  func testLaunchPerformance() throws {
+    if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+      measure(metrics: [XCTApplicationLaunchMetric()]) {
+        XCUIApplication().launch()
+      }
+    }
+  }
+}
